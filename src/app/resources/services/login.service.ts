b14c614ae36f132783b273dbe00a867715ebacf7
Andre da Silva Carmona <104648375+DevCarmona@users.noreply.github.com>
2024-02-21 16:01:22 -0300

    Criando authguard

--- src/app/resources/services/login.service.ts
+++ src/app/resources/services/login.service.ts
@@ -1,17 +1,18 @@
 import { Injectable } from '@angular/core';
 import { RequestLogin } from '../models/RequestLogin';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable} from 'rxjs';
 import { ResponseLogin } from '../models/ResponseLogin';
+import { tap } from 'rxjs/operators';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
-
-  constructor(private httpCliente: HttpClient) { }
+  constructor(private httpCliente: HttpClient, private authService: AuthService) { }
 
   public doLogin(requestLogin: RequestLogin): Observable<ResponseLogin>  {
-    return this.httpCliente.post<ResponseLogin>('http://localhost:8080/api/login', requestLogin);
+    return this.httpCliente.post<ResponseLogin>('http://localhost:8080/api/login', requestLogin).pipe(tap((loginResponse) => (this.authService.loginResponse = loginResponse)));
   }
 }
